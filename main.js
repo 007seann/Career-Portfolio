9c218d20f3ca7b0eb099f0c7e9eeeccc072051d6
Sean CHOI <007seann@gmail.com>
2022-07-12 14:27:51 +0300

    JS transparent

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
+
 document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
@@ -27,6 +28,30 @@ const contact = document.querySelector('.home__contact');
 contact.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
+
+// Make home slowly fade to be trasparent as the window scrolls down
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// const homeAvatar = document.querySelector('.home__avatar');
+// const homeTitle = document.querySelector('.home__title');
+// const homeDescription = document.querySelector('.home__description');
+
+// document.addEventListener('scroll', () => {
+//   console.log(window.scrollY);
+//   if ((window.scrollY = 0)) {
+//     homeAvatar.classList.add('home__avatar-1');
+//   } else if ((window.scrollY = homeHeight / 2)) {
+//     homeAvatar.classList.add('home__avatar-2');
+//   } else if ((window.scrollY = homeHeight)) {
+//     homeAvatar.classList.add('home__avatar-3');
+//   }
+// });
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
